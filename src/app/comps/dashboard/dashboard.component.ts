a01dca24925313f7b05fe7ae093fc2cbde4d6067
Frank Mastronardi <mastronardif@gmail.com>
2022-02-06 14:11:42 -0500

    added comps to mat grid

--- src/app/comps/dashboard/dashboard.component.ts
+++ src/app/comps/dashboard/dashboard.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { FormControl, FormGroup } from '@angular/forms';
+import * as gDummy from '../../dummydata/dummys';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+
+  formSection = new FormGroup({
+    //firstName: new FormControl(''),
+
+    address: new FormGroup({
+      country: new FormControl(''),
+      city: new FormControl(''),
+    }),
+  });
+
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
@@ -30,4 +42,33 @@ export class DashboardComponent {
   );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit(): void {
+    this.formSection = new FormGroup({
+      // firstName: new FormControl(''),
+
+      // stackDetails: new FormGroup({
+      //   stack: new FormControl(''),
+      //   experience: new FormControl(''),
+      // }),
+
+      address: new FormGroup({
+        company: new FormControl(''),
+        firstName: new FormControl(''),
+        lastName: new FormControl(''),
+        address: new FormControl(''),
+        address2: new FormControl(''),
+        city: new FormControl(''),
+        state: new FormControl(''),
+        postalCode: new FormControl(''),
+        shipping: new FormControl(''),
+      }),
+    });
+    this.patch('address', gDummy.mockData.address);
+  }
+
+  patch(section: string, data: any) {
+    console.log('patch(section:string, data: any)');
+    const dummyAddress = data;
+    this.formSection.patchValue(dummyAddress);
+  }
 }
